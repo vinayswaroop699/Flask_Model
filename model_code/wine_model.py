db653fdc4d8033387fba7edbc8f8e2daa044b2c8
vinayswaroop699 <vinayswaroop699@gmail.com>
2025-07-19 19:28:58 +0300

    test_size to 0.1

--- model_code/wine_model.py
+++ model_code/wine_model.py
@@ -16,7 +16,7 @@ X = data.drop('quality', axis=1)
 y = data['quality']
 
 # Split into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.6, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 
 # Create and train the classifier
 clf = RandomForestClassifier(random_state=42)
